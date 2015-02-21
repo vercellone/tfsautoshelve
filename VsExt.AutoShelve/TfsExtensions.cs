16f41ad120a866a2193cbb198490346e4ec5ce4d
Jason Vercellone <vercellone@users.noreply.github.com>
2015-02-21 02:47:16 -0600

    Addresses GitHub Issues 4,5, and 6.  There is now a single TFS Auto Shelve service regardless of the number of Visual Studio instances open.  It no longer verifies the open solution is in a TFS workspace.  Instead it shelves at the interval only if changes are detected.  First check is # of most recently shelved items vs. # of pending changes.  If that # matches, it searches for any single pending change whose UploadHashValue differs from it's local file content's current HashValue.

---
+++ VsExt.AutoShelve/TfsExtensions.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.TeamFoundation.VersionControl.Client;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VsExt.AutoShelve
+{
+    public static class TfsExtensions
+    {
+        public static bool HaveChanged(this PendingChange[] changes)
+        {
+            foreach (var change in changes)
+            {
+                if (!change.UploadHashValue.SequenceEqual(GetMD5HashValue(change)))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static byte[] GetMD5HashValue(PendingChange change)
+        {
+            if (File.Exists(change.LocalItem))
+            {
+                return MD5.Create().ComputeHash(File.ReadAllBytes(change.LocalItem));
+            }
+            else
+            {
+                return change.HashValue;
+            }
+        }
+
+        public static string GetDomain(this string name)
+        {
+            int stop = name.IndexOf("\\");
+            return (stop > -1) ? name.Substring(0, stop) : string.Empty;
+        }
+
+        public static string GetLogin(this string name)
+        {
+            int stop = name.IndexOf("\\");
+            return name.Substring(stop + 1);
+            //return (stop > -1) ? name.Substring(stop + 1, name.Length - stop - 1) : string.Empty;
+        }
+
+    }
+}
