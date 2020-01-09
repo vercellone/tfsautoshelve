59ae0243c5d7fab82deb5c9e87e97d382a5af9a1
Jason Vercellone <vercellone@users.noreply.github.com>
2020-01-09 01:13:19 -0600

    VSIX ID updated; published a new 2019 extension rather than update the existing 2017 extension as planned

--- VsExt.AutoShelve/Packaging/Guids.cs
+++ VsExt.AutoShelve/Packaging/Guids.cs
@@ -4,7 +4,7 @@ namespace VsExt.AutoShelve.Packaging
 {
     static class GuidList
     {
-        public const string GuidAutoShelvePkgString = "8016DBDE-8330-4802-9B1C-1E0AD1102A24";
+        public const string GuidAutoShelvePkgString = "E2FEEB86-AEB7-4ABB-A3BA-88457F4F7F27";
         private const string GuidAutoShelveCmdSetString = "4375B001-B852-4FA2-BD87-3106ACECDBBF";
 
         public static readonly Guid GuidAutoShelveCmdSet = new Guid(GuidAutoShelveCmdSetString);
